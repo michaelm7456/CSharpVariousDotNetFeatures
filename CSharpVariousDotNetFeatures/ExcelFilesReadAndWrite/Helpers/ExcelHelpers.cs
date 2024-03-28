9404e15cacc91175a589fb31ab514396616cc017
Michael Murphy <michaelm23119@gmail.com>
2024-03-28 16:50:45 +0000

    Added 'Excel Files Read/Write' example, using the 'EPPlus' library.

---
+++ CSharpVariousDotNetFeatures/ExcelFilesReadAndWrite/Helpers/ExcelHelpers.cs
@@ -0,0 +1,81 @@
+﻿using System.Drawing;
+using OfficeOpenXml;
+
+namespace ExcelFilesReadAndWrite.Helpers
+{
+    public class ExcelHelpers
+    {
+        public FileInfo testFile = new(@"C:\\Repositories\\GitHub Repositories\\CSharpVariousDotNetFeatures\\CSharpVariousDotNetFeatures\\ExcelFilesReadAndWrite\\ExcelFiles\\Excel_Test_File.xlsx");
+
+        public static List<PersonModel> GetSetupData()
+        {
+            List<PersonModel> output =
+            [
+                new() { Id = 1, FirstName = "John", LastName = "Smith" },
+                new() { Id = 2, FirstName = "Jane", LastName = "Doe" },
+                new() { Id = 3, FirstName = "Alan", LastName = "MacMillan" }
+            ];
+
+            return output;
+        }
+
+        public static async Task<List<PersonModel>> LoadExcelFile(FileInfo file)
+        {
+            List<PersonModel> output = [];
+
+            using var package = new ExcelPackage(file);
+
+            await package.LoadAsync(file);
+
+            var worksheet = package.Workbook.Worksheets[0];
+
+            int row = 3;
+            int col = 1;
+
+            while (string.IsNullOrWhiteSpace(worksheet.Cells[row, col].Value?.ToString()) == false)
+            {
+                PersonModel p = new()
+                {
+                    Id = int.Parse(worksheet.Cells[row, col].Value.ToString()),
+                    FirstName = worksheet.Cells[row, col + 1].Value.ToString(),
+                    LastName = worksheet.Cells[row, col + 2].Value.ToString()
+                };
+                output.Add(p);
+                row++;
+            }
+
+            return output;
+        }
+
+        public static async Task SaveExcelFile(List<PersonModel> people, FileInfo file)
+        {
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+
+            using var package = new ExcelPackage(file);
+
+            //Apply Naming to the First Tab.
+            var worksheet = package.Workbook.Worksheets.Add("MainReport");
+
+            //Apply formatting to range of Cells.
+            var range = worksheet.Cells["A2"].LoadFromCollection(people, true);
+            range.AutoFitColumns();
+
+            //Format header.
+            worksheet.Cells["A1"].Value = "Report Heading";
+            worksheet.Cells["A1:C1"].Merge = true;
+            worksheet.Column(1).Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+            worksheet.Row(1).Style.Font.Size = 24;
+            worksheet.Row(1).Style.Font.Color.SetColor(Color.Blue);
+
+            worksheet.Row(2).Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+            worksheet.Row(2).Style.Font.Bold = true;
+
+            worksheet.Column(3).Width = 20;
+
+            await package.SaveAsync();
+        }
+    }
+}
---
+++ CSharpVariousDotNetFeatures/ExcelFilesReadAndWrite/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExcelFilesReadAndWrite.Helpers;
+using OfficeOpenXml;
+
+ExcelHelpers _ExcelHelpers = new();
+
+//We must declare License as 'Non Commercial' to indicate that we're using this for personal projects as opposed to Commercial purposes.
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+//Generates Excel file based on our sample data.
+await ExcelHelpers.SaveExcelFile(ExcelHelpers.GetSetupData(), _ExcelHelpers.testFile);
+
+List<PersonModel> peopleFromExcel = await ExcelHelpers.LoadExcelFile(_ExcelHelpers.testFile);
+foreach (var p in peopleFromExcel)
+{
+    Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName}");
+}
+
+Console.ReadLine();
