cea808d06702bb7915ca3f24057399840c2ed6ed
Michael Murphy <mmurph33@caledonian.ac.uk>
2024-11-28 17:28:01 +0000

    Added 'CustomerDTO' class, to test deserialising sample json payloads, and mapping this to sample dto class.

---
+++ CSharpVariousDotNetFeatures/FileImports/CustomerDTO.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FileImports
+{
+    public class CustomerDTO
+    {
+        [JsonPropertyName("id")]
+        public required long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        [JsonPropertyName("email")]
+        public required string Email { get; set; }
+
+        [JsonPropertyName("age")]
+        public required int Age { get; set; }
+
+        [JsonPropertyName("address")]
+        public required AddressDTO Address { get; set; }
+
+        [JsonPropertyName("is_active")]
+        public required bool IsActive { get; set; }
+
+        [JsonPropertyName("signup_date")]
+        public required DateTime SignupDate { get; set; }
+
+        [JsonPropertyName("last_login")]
+        public DateTime? LastLogin { get; set; }
+
+        [JsonPropertyName("subscription_level")]
+        public string? SubscriptionLevel { get; set; }
+
+        [JsonPropertyName("preferred_language")]
+        public string? PreferredLanguage { get; set; }
+
+        [JsonPropertyName("contact_numbers")]
+        public required List<ContactNumberDTO> ContactNumbers { get; set; }
+    }
+
+    public class AddressDTO
+    {
+        [JsonPropertyName("street")]
+        public required string Street { get; set; }
+
+        [JsonPropertyName("city")]
+        public required string City { get; set; }
+
+        [JsonPropertyName("postal_code")]
+        public required string PostalCode { get; set; }
+    }
+
+    public class ContactNumberDTO
+    {
+        [JsonPropertyName("type")]
+        public required string Type { get; set; }
+
+        [JsonPropertyName("number")]
+        public required string Number { get; set; }
+    }
+}
--- CSharpVariousDotNetFeatures/FileImports/Program.cs
+++ CSharpVariousDotNetFeatures/FileImports/Program.cs
@@ -1,12 +1,25 @@
-﻿var fileName = "samplepayload.json";
+﻿using FileImports;
+using System.Text.Json;
+
+var fileName = "samplepayload.json";
 
 var payloadFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine($"Importing sample payload at path: CSharpVariousDotNetFeatures\\CSharpVariousDotNetFeatures\\FileImports\\samplepayload.json");
 
 var payload = File.ReadAllText(payloadFilePath);
+var dto = JsonSerializer.Deserialize<CustomerDTO>(payload);
+
 Console.WriteLine();
 Console.WriteLine("Payload contents are:");
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine(payload);
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Payload has been deserialised successfully into DTO.");
+Console.ForegroundColor = ConsoleColor.White;
 
 Console.ReadLine();
