58657ff6f45643371204e4d81f0d5530a239185f
Michael Murphy <mmurph33@caledonian.ac.uk>
2024-11-28 17:05:40 +0000

    Added 'FileImports' project, for purposes of quickly analysing and testing json payload examples.

---
+++ CSharpVariousDotNetFeatures/FileImports/Program.cs
@@ -0,0 +1,12 @@
+﻿var fileName = "samplepayload.json";
+
+var payloadFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+Console.WriteLine($"Importing sample payload at path: CSharpVariousDotNetFeatures\\CSharpVariousDotNetFeatures\\FileImports\\samplepayload.json");
+
+var payload = File.ReadAllText(payloadFilePath);
+Console.WriteLine();
+Console.WriteLine("Payload contents are:");
+Console.WriteLine();
+Console.WriteLine(payload);
+
+Console.ReadLine();
